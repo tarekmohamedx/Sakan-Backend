5beb44822b6510bdfe45302ce22cf595fe8530e0
abdelrhmanBa7Ba7 <abdelrhmanhisham199@gmail.com>
2025-07-05 01:50:17 +0300

    logic updated

--- Sakan.Application/DTOs/BookingRequestsDTO.cs
+++ Sakan.Application/DTOs/BookingRequestsDTO.cs
@@ -8,12 +8,16 @@ namespace Sakan.Application.DTOs
 {
     public class BookingRequestsDTO
     {
+        public string GuestId { get; set; }
+        public string HostId { get; set; }
         public int BookingRequestId { get; set; }
         public string ListingTitle { get; set; }
         public decimal? BedPrice { get; set; }
         public string ListingLocation { get; set; }
         public DateTime FromDate { get; set; }
         public DateTime ToDate { get; set; }
+
+        public String Status { get; set; } // "Pending", "Accepted", "Rejected"
 
     }
 }
