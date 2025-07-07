d33aa4d75d5af0773f9e0d4d9b474e73fee4c475
Eman Sameh <emansameh143@gmail.com>
2025-07-07 19:09:28 +0300

    add approve hosts in admin page

---
+++ Sakan.Application/Interfaces/Admin/IAdminHostsService.cs
@@ -0,0 +1,15 @@
+﻿using Sakan.Application.DTOs.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakan.Application.Interfaces.Admin
+{
+    public interface IAdminHostsService
+    {
+        Task<List<HostAdminViewDto>> GetAllHostsAsync();
+        Task<string> HandleHostApprovalAsync(AdminHostApprovalDto dto);
+    }
+}
